9663172a2ddccaed69446222af4fe9cb9de8f6ec
Franyi Liriano <franyiliriano429@gmail.com>
2024-05-07 16:10:36 -0400

    Patrón de repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV7.AccesoDatos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task Agregar(T entidad)
+        {
+            await dbSet.AddAsync(entidad); //Equivale a un insert into en la tabla.
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await dbSet.FindAsync(id); //Equivale a un select * from solo filtra por Id
+
+        }
+
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro); //Select * from where ...
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp);     //ejemplo "Categoría,Marca" 
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null,
+            string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro); //Select * from where ...
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp);     //ejemplo "Categoría,Marca" 
+                }
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Remover(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverRango(IEnumerable<T> entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SistemaInventarioV7/Program.cs
+++ SistemaInventarioV7/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
 using SistemaInventarioV7.AccesoDatos.Data; 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddScoped<IUnidadTrabajo, IUnidadTrabajo>();
 
 var app = builder.Build();
 
