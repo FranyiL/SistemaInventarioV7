892b6da265b49789758ad08624351bbd1025f3fc
Franyi Liriano <franyiliriano429@gmail.com>
2024-05-17 09:58:20 -0400

    Registro de Usuarios

--- SistemaInventario.Utilidades/DS.cs
+++ SistemaInventario.Utilidades/DS.cs
@@ -8,8 +8,12 @@ namespace SistemaInventario.Utilidades
 {
     public static class DS
     {
-        public const string Exitosa = "Exitosa";   
+        public const string Exitosa = "Exitosa";
         public const string Error = "Error";
         public const string ImagenRuta = @"\imagenes\producto\";
+
+        public const string Rol_Admin = "Admin";
+        public const string Rol_Cliente = "Cliente";
+        public const string Rol_Inventario = "Inventario";
     }
 }
--- SistemaInventarioV7/Program.cs
+++ SistemaInventarioV7/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Core.Types;
 using SistemaInventario.AccesoDatos.Repositorio;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Utilidades;
 using SistemaInventarioV7.AccesoDatos.Data; 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +15,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
