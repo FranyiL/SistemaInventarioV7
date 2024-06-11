48cb97a62382aab771515e8506056aacfe76673b
Franyi Liriano <franyiliriano429@gmail.com>
2024-06-11 13:03:08 -0400

    Carro de Compras - Modelo y Repositorio

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -21,8 +21,10 @@ namespace SistemaInventarioV7.AccesoDatos.Data
         public DbSet<Inventario> Inventarios{ get; set; }
         public DbSet<InventarioDetalle> InventarioDetalles { get; set; }
         public DbSet<KardexInventario> KardexInventarios { get; set; }
-
         public DbSet<Compania> Companias { get; set; }
+        public DbSet<CarroCompra> CarroCompras { get; set; }
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalles { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
---
+++ SistemaInventario.AccesoDatos/Repositorio/OrdenDetalleRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventarioV7.AccesoDatos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class OrdenDetalleRepositorio : Repositorio<OrdenDetalle>, IOrdenDetalleRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrdenDetalleRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Actualizar(OrdenDetalle ordenDetalle)
+        {
+            //Para actualizar todo no desglosamos nada y enviamos todo el carro de compra a su método update.
+            _db.Update(ordenDetalle);
+        }
+    }
+}
